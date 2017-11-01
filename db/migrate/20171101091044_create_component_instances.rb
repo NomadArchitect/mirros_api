963a60be2dc5b6e7f82a085c1a10d0dbe55fe23f
Marco Roth <marco.roth@intergga.ch>
2017-11-01 10:49:36 +0100

    add field component_id to ComponentInstance

--- db/migrate/20171101091044_create_component_instances.rb
+++ db/migrate/20171101091044_create_component_instances.rb
@@ -1,7 +1,8 @@
 class CreateComponentInstances < ActiveRecord::Migration[5.1]
   def change
     create_table :component_instances do |t|
-
+      t.integer :component_id
+      
       t.timestamps
     end
   end
