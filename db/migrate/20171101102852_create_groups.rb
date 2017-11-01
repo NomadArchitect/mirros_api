42c95e62dec77e9a178976d378f9b3d4d12a0b86
Marco Roth <marco.roth@intergga.ch>
2017-11-01 11:56:40 +0100

    remove source_id and component_id from Group

--- db/migrate/20171101102852_create_groups.rb
+++ db/migrate/20171101102852_create_groups.rb
@@ -2,8 +2,6 @@ class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
       t.string :name
-      t.integer :source_id
-      t.integer :component_id
       t.integer :category_id
 
       t.timestamps
