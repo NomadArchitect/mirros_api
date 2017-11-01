1cb0eef276c1ad3cd6af64bc3c72c299689475be
Marco Roth <marco.roth@intergga.ch>
2017-11-01 11:37:04 +0100

    add relation between Category and Group

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,5 @@ class Category < ApplicationRecord
 
   has_many :components
   has_many :sources
+  has_many :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   has_many :components
   has_many :sources
+  belongs_to :category
 end
