063719a8b2f2eb66d8ef1499338b4fa2fe256b9e
Marco Roth <marco.roth@intergga.ch>
2017-12-20 19:16:56 +0100

    add method for all engines

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,4 +2,8 @@ class Source < ApplicationRecord
   has_many :source_instances, dependent: :destroy
   has_and_belongs_to_many :groups
   belongs_to :category
+
+  def self.all
+    Rails::Engine.subclasses-[ActionView::Railtie, ActionCable::Engine]
+  end
 end
