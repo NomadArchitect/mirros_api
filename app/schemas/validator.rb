6b54abb808bec31ed757ba24a42b7b5e0c873c34
Marco Roth <marco.roth@intergga.ch>
2017-12-21 18:28:49 +0100

    make json-schema validator as a function

---
+++ app/schemas/validator.rb
@@ -0,0 +1,19 @@
+require "json"
+require "json-schema"
+
+
+def validate_schema(schema = {}, json)
+  schema = File.read(schema)
+  json = JSON.parse(json.to_s).to_hash
+
+  validator = JSON::Validator.fully_validate(schema, json)
+
+  if validator.count == 0
+    return { valid: true, errors: [] }
+  else
+    return { valid: false, errors: validator }
+  end
+end
+
+
+puts validate_schema("components.json", File.read("component.json"))
