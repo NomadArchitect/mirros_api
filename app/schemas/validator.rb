bfe9060d4537221a92a396225ce51964eda52504
Marco Roth <marco.roth@intergga.ch>
2018-03-16 12:08:27 +0100

    update json-schema validator

--- app/schemas/validator.rb
+++ app/schemas/validator.rb
@@ -1,10 +1,9 @@
 require "json"
 require "json-schema"
 
-
 def validate_schema(schema = {}, json)
   schema = File.read(schema)
-  json = JSON.parse(json.to_s).to_hash
+  json = JSON.parse(File.read(json).to_s).to_hash
 
   validator = JSON::Validator.fully_validate(schema, json)
 
@@ -15,5 +14,7 @@ def validate_schema(schema = {}, json)
   end
 end
 
+json_file = "app/schemas/widget.json"
+schema_file = "app/schemas/widgets.json"
 
-puts validate_schema("widgets.json", File.read("widget.json"))
+puts validate_schema(schema_file, json_file)
