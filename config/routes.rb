f411b5424561efbb807be512a8e006ccfd4d069d
Tobias Grasse <mail@tobias-grasse.net>
2018-03-19 15:42:01 +0100

    Add services relationship to widget resource routes
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   jsonapi_resources :widgets do
     jsonapi_related_resources :widget_instances
+    jsonapi_related_resources :services
   end
   jsonapi_resources :sources do
     jsonapi_related_resources :source_instances
