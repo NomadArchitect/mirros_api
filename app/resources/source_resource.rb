4b800d169058c50c8132f972d1c107a10037c277
Tobias Grasse <mail@tobias-grasse.net>
2018-03-19 17:47:40 +0100

    Make widget and source resources immutable
    Installation/deinstallation will be performed through a system API endpoint, user-generated settings operate on individual instances.
    
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

--- app/resources/source_resource.rb
+++ app/resources/source_resource.rb
@@ -1,4 +1,6 @@
 class SourceResource < JSONAPI::Resource
+  immutable
+  
   attributes :name, :author, :version
   has_many :source_instances,
   always_include_linkage_data: true
--- app/resources/widget_resource.rb
+++ app/resources/widget_resource.rb
@@ -1,4 +1,6 @@
 class WidgetResource < JSONAPI::Resource
+  immutable
+
   attributes :name, :icon, :version, :creator, :website, :languages, :installed
   has_many :services
   has_many :widget_instances
