40c706037ad7487faad6dad5faac72e991cc38c4
Marco Roth <marco.roth@intergga.ch>
2018-03-28 14:32:50 +0200

    remove categories reference and CategoryHelper

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,6 @@
 class Group < ApplicationRecord
   has_and_belongs_to_many :widgets
   has_and_belongs_to_many :sources
-  has_and_belongs_to_many :categories
 
   validates :name, uniqueness: true
 
