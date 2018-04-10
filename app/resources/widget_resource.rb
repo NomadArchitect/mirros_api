243c862ebcd33daaa474e42d92f7c3ffead53324
Tobias Grasse <tg@glancr.de>
2018-04-10 16:22:16 +0200

    Change update hook to after_update
    Otherwise @model would still have the old version number

--- app/resources/widget_resource.rb
+++ app/resources/widget_resource.rb
@@ -2,7 +2,7 @@ class WidgetResource < JSONAPI::Resource
 
   include Installable
   after_create :install
-  before_update :update
+  after_update :update
   before_remove :uninstall
 
   primary_key :slug
