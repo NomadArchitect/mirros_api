368bfd811842f4314bad104671cf8a49bd53ec41
Tobias Grasse <mail@tobias-grasse.net>
2018-04-11 18:15:21 +0200

    [WIP] Working installation/deinstallation routines
    Using a secondary Gemfile opened a bag of hurt, since Bundler’s API *looks* like you can configure lockfiles but it has so many dependencies and hardcoded stuff, things just break. Use the main Gemfile for now and devise some update method to keep engine gems for later.
    
    Signed-off-by: Tobias Grasse <mail@tobias-grasse.net>

--- Gemfile
+++ Gemfile
@@ -71,6 +71,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 # gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
-# Engine gems are located in a secondary file to prevent overwriting installation state on system update.
-eval_gemfile('Gemfile.local')
--- app/controllers/concerns/installable.rb
+++ app/controllers/concerns/installable.rb
@@ -25,25 +25,33 @@ module Installable
       injector = Bundler::Injector.new([dep])
 
       # Injector._append_to expects Pathname objects instead of path strings.
-      local_gemfile = Pathname.new(File.absolute_path('Gemfile.local'))
+      gemfile = Pathname.new(File.absolute_path('Gemfile'))
       lockfile = Pathname.new(File.absolute_path('Gemfile.lock'))
-      new_deps = injector.inject(local_gemfile, lockfile)
+
+      new_deps = injector.inject(gemfile, lockfile)
 
     rescue Bundler::Dsl::DSLError => e
       bundler_error(e)
     end
 
+    # default_lock = Bundler.default_lockfile
+    #
     installer = Bundler::Installer.new(Bundler.root, Bundler.definition)
-    installer.run({})
+    installer.run({'jobs' => 5})
 
     # TODO: Validate @model against downloaded extension info (TBD)
-    require_type
+    require_gem
     # TODO: Service registration etc.
+    blubb = installed?
+
+    merde = "@gem".safe_constantize
   end
 
   # Checks whether the given extension resource is properly installed.
   def installed?
     Gem.loaded_specs.keys.any?(@model.attributes['name'])
+    blubb = Gem.loaded_specs.keys.any?(@model.attributes['name'])
+    blubb
   end
 
   def uninstall
@@ -62,16 +70,16 @@ module Installable
     search = /gem "#{engine['name']}", "= [0-9].[0-9].[0-9]"/
     replace = "gem \"#{engine['name']}\", \"= #{engine['version']}\""
 
-    tmp = Tempfile.new(['Gemfile.local', '.tmp'], Rails.root.to_s + '/tmp')
-    tmp.write(File.read(Rails.root.to_s + '/Gemfile.local').dup.gsub(search, replace))
+    tmp = Tempfile.new(['Gemfile', '.tmp'], Rails.root.to_s + '/tmp')
+    tmp.write(File.read(Rails.root.to_s + '/Gemfile').dup.gsub(search, replace))
     tmp.rewind
-    FileUtils.copy(tmp, Rails.root.to_s + "/Gemfile.local")
+    FileUtils.copy(tmp, Rails.root.to_s + "/Gemfile")
     tmp.close!
 
     installer = Bundler::Installer.new(Bundler.root, Bundler.definition)
     installer.run({})
 
-    require_type
+    Bundler.require(*Rails.groups)
     # TODO: service re-registration etc. necessary?
   end
 
@@ -86,36 +94,38 @@ module Installable
   def uninstall_gem
     # Bundler has no remove method yet, so we need to manually remove the gem's line from the Gemfile.
     search_text = /gem "#{@gem}"/
-    tmp = Tempfile.new(['Gemfile.local', '.tmp'])
+    tmp = Tempfile.new(['Gemfile', '.tmp'])
 
-    File.open("Gemfile.local", 'r') do |file|
+    File.open("Gemfile", 'r') do |file|
       file.each_line do |line|
-        tmp.write(line) unless line =~ search_text || line =~ /#/
+        tmp.write(line) unless line =~ search_text || line =~ /# Added/ || line =~/^\n$/
       end
     end
 
     tmp.rewind
-    FileUtils.copy(tmp, "Gemfile.local")
+    FileUtils.copy(tmp, "Gemfile")
     tmp.close!
 
     # Once the extension gem is removed, clear its dependencies if they are no longer required (eq. `bundle clean`)
-    extensions = Bundler.setup(@extension_type)
+    extensions = Bundler::Runtime.new(Bundler.root, Bundler::Definition.build(Bundler.default_gemfile, Bundler.default_lockfile, nil))
     extensions.clean
   end
 
   # @param [Object] error An optional Error object that has the methods message and status_code.
   def bundler_error(error = nil)
-    uninstall_gem(@gem) # Roll back changes made to Gemfile.local
+    uninstall_gem # Roll back changes made to Gemfile_extensions
     msg = "Error while installing extension #{@gem})"
     msg += error.nil? ? ": #{error.message}, code: #{error.status_code}" : ""
     raise JSONAPI::Exceptions::InternalServerError.new(msg)
   end
 
-  def require_type
+  def require_gem
     begin
       # Installed extensions are scoped by group. Reload just this group instead of all gems.
-      Bundler.require(@extension_type)
-    rescue Bundler::LoadError => e
+      rt = Bundler::Runtime.new(Bundler.root, Bundler::Definition.build(Bundler.default_gemfile, Bundler.default_lockfile, nil))
+      rt.require(*Rails.groups)
+        #Bundler.require(@extension_type)
+    rescue Bundler::GemRequireError => e
       bundler_error(e)
     end
   end
