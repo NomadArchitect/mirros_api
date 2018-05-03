47f9cc7c4aee890b940d3815adcef947cd8fd0a4
Tobias Grasse <tg@glancr.de>
2018-05-03 16:59:57 +0200

    Add callbacks for (un/re)scheduling jobs to SIResource
    Source instances are the primary resource for data refresh jobs, so we need to schedule the refresh on installation (== creation), reschedule after updates (the source code might have changed) and unschedule after its deletion (the engine is no longer present).
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/resources/source_instance_resource.rb
+++ app/resources/source_instance_resource.rb
@@ -1,5 +1,21 @@
 class SourceInstanceResource < JSONAPI::Resource
+  after_create :add_to_scheduler
+  after_update :update_scheduler
+  after_remove :remove_from_scheduler
   attributes :title, :configuration
   has_one :source
   has_many :widget_instances, through: :instance_associations
+end
+
+def add_to_scheduler
+  DataRefresher.schedule(@model)
+end
+
+def update_scheduler
+  DataRefresher.unschedule(@model)
+  DataRefresher.schedule(@model)
+end
+
+def remove_from_scheduler
+  DataRefresher.unschedule(@model)
 end
