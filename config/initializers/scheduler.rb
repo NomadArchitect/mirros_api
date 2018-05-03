21bad200dfcca7c90c1c6db227c66cf0eb9fa997
Tobias Grasse <tg@glancr.de>
2018-05-03 14:52:13 +0200

    Use correct check for Rails console env
    Do not run the scheduler if Rails is run from the console to prevent conflicts.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,6 +4,6 @@
 require 'rufus-scheduler'
 
 # only schedule when not running from the Ruby on Rails console or from a rake task
-unless defined?(Rails::Console) || File.split($PROGRAM_NAME).last == 'rake'
+unless Rails.const_defined? 'Console' || File.split($PROGRAM_NAME).last == 'rake'
   MirrOSApi::DataRefresher.schedule_all
 end
