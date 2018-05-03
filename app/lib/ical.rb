eb785c3ce3fc2e21d2f82fdfb6187a9beea1a108
Tobias Grasse <tg@glancr.de>
2018-05-03 20:17:48 +0200

    Implement exemplary engine stub for ical source
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/ical.rb
+++ app/lib/ical.rb
@@ -1,15 +1,72 @@
 module Ical
   class Engine
-    def self.schedule_rate
-      '5m'
+
+    REFRESH_INTERVAL = '5m'.freeze
+
+    # @return [String]
+    def self.refresh_interval
+      REFRESH_INTERVAL
     end
 
-    def initialize()
-
+# @param [Hash] configuration
+# @param [Hash] subresources
+    def initialize(configuration, subresources = {})
+      @username = configuration['user']
+      @password = configuration['password']
+      @url = configuration['url']
+      @ical = CalendarTest.new(@url)
     end
 
-    def refresh
-      "I refresh the data for #{self.class} every #{self.class.schedule_rate}"
+    def config_valid?
+      # Public calendars do not need credentials.
+      res = HTTParty.head(@url)
+      true unless res.code != 200
+    end
+
+    def list_subresources
+      @ical.calendars
+    end
+
+    def fetch_data(subresources)
+      map = Hash.new
+      subresources.each do |subresource|
+        map[subresource] = @ical.events(subresource)
+      end
+      return map
     end
   end
+
+  class CalendarTest
+    def initialize(url)
+      @cal_map = Hash.new
+      content = HTTParty.get(url)
+      Icalendar::Parser.new(content).parse.map do |cal|
+        calendar_name = cal.x_wr_calname.empty? ? cal.name : cal.x_wr_calname.first.value
+        @cal_map[calendar_name] = cal
+      end
+    end
+
+    def calendars
+      @cal_map.keys
+    end
+
+    def events(calendar)
+      subset = @cal_map[calendar].events.select do |event|
+        event.dtstart >= Date.today && event.dtstart <= Date.today + 14
+      end
+
+      unless subset.empty?
+        event_hashes = subset.map do |event|
+          {
+            dtstart: event.dtstart.value,
+            dtend: event.dtend.value,
+            summary: event.summary.value,
+            description: event.description.value
+          }
+        end
+        event_hashes
+      end
+    end
+  end
+
 end
