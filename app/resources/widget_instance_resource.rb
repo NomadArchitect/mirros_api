936eeb5eb994dfb8c017b5f6f997afbc4a3b9b48
Tobias Grasse <mail@tobias-grasse.net>
2018-06-14 14:31:49 +0000

    Resolve "Add groups linkage data to WidgetResource and SourceResource"

--- app/resources/widget_instance_resource.rb
+++ app/resources/widget_instance_resource.rb
@@ -1,6 +1,6 @@
 class WidgetInstanceResource < JSONAPI::Resource
   attributes :configuration, :position
-  has_one :widget
+  has_one :widget, always_include_linkage_data: true
   has_many :instance_associations
   has_many :source_instances, through: :instance_associations
 end
