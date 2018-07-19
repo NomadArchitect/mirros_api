0113757d6453c78d2e3dfc4d05b7703035037f17
Tobias Grasse <tg@glancr.de>
2018-07-19 16:24:40 +0200

    Include data from last refresh in instance resource
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/resources/source_instance_resource.rb
+++ app/resources/source_instance_resource.rb
@@ -2,8 +2,8 @@ class SourceInstanceResource < JSONAPI::Resource
   after_create :add_to_scheduler
   after_update :update_scheduler
   after_remove :remove_from_scheduler
-  attributes :title, :configuration
   has_one :source
+  attributes :title, :configuration, :data
   has_many :widget_instances, through: :instance_associations
 end
 
