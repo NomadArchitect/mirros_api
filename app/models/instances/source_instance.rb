36685c694f1c86c0fc84c8e98bf86c53aefe421c
Tobias Grasse <tg@glancr.de>
2018-07-26 13:42:37 +0200

    Add get_records method on SourceInstance model
    Retrieves all records found for the base source model.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/instances/source_instance.rb
+++ app/models/instances/source_instance.rb
@@ -2,4 +2,16 @@ class SourceInstance < Instance
   belongs_to :source
   has_many :instance_associations, dependent: :destroy
   has_many :widget_instances, through: :instance_associations
+  def get_records
+    records = {}
+    self.source.groups.map do |group|
+      schema = group_schema(group)
+      records[group.name] = schema.where(source_instance: self.id)
+    end
+    records
+  end
+  private
+    def group_schema(group)
+      "#{self.source.id.capitalize}::#{group.name.capitalize}".safe_constantize
+    end
 end
