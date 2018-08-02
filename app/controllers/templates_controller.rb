965fea059270ef22e0225963207c73651f4099a2
Tobias Grasse <tg@glancr.de>
2018-08-02 16:36:45 +0200

    Add template retrieval logic to TemplatesController
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,2 +1,20 @@
+require 'bundler/cli'
+require 'bundler/cli/show'
+
 class TemplatesController < ApplicationController
+
+  def show
+    extension = params[:extension]
+    application = params[:application]
+    extension_path = Bundler::CLI::Show.new({}, extension).run
+
+    if extension_path
+      file_path = "#{extension_path}/app/assets/#{extension}-#{application}.vue"
+      send_file(file_path)
+
+    else
+      head :not_found
+    end
+
+  end
 end
