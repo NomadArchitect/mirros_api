75f1d9b77aebc9393a5567d0a031343084ec3da3
Tobias Grasse <tg@glancr.de>
2018-08-08 14:09:19 +0200

    Add group relation to WidgetInstance and resource
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/instances/widget_instance.rb
+++ app/models/instances/widget_instance.rb
@@ -1,5 +1,6 @@
 class WidgetInstance < Instance
   belongs_to :widget
+  has_many :groups, through: :widget
   has_many :instance_associations, dependent: :destroy
   has_many :source_instances, through: :instance_associations
 end
