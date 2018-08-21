e426fad87b892df990a3f330ff7c59711648e1e5
Tobias Grasse <tg@glancr.de>
2018-08-21 14:15:42 +0200

    Modify template routes to handle all ext. assets
    Param :type matches directories within an extension’s app/assets/ directory.
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,17 +1,16 @@
-class TemplatesController < ApplicationController
+class AssetsController < ApplicationController
 
   def show
     gem = Gem.loaded_specs[params[:extension]]
     return head :not_found if gem.nil?
 
     extension_path = gem.full_gem_path
-    file_path = "#{extension_path}/app/assets/#{params[:application]}.vue"
+    file_path = "#{extension_path}/app/assets/#{params[:type]}/#{params[:file]}"
 
     if Pathname.new(file_path).exist?
       send_file(file_path)
     else
       head :not_found
     end
-
   end
 end
