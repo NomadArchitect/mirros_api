2281d1e3771f43378b52cc29ec005d13eec2adbd
Tobias Grasse <tg@glancr.de>
2018-10-01 10:28:51 +0200

    Add route and controller method to run setup

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -4,6 +4,33 @@ class SystemController < ApplicationController
     render json: { meta: System.info }
   end
 
+  def run_setup
+    connection = Setting.find_by_slug('network_connectiontype').value
+    SettingExecution::Network.close_ap
+
+    if connection == 'wlan'
+      begin
+        result = SettingExecution::Network.connect
+        success = true
+      rescue ArgumentError => e
+        result = e.message
+        success = false
+      end
+    else # Using LAN, no further setup required
+      result = 'Using LAN connection'
+      success = true
+    end
+
+    # TODO: Evaluate: Since nmcli should not return before the connection is
+    # established, we should not need a retry loop here.
+    if success && System.online?
+      SettingExecution::Personal.send_setup_email
+    else
+      SettingExecution::Network.open_ap
+    end
+
+    render json: { success: success, result: result }
+  end
   def setting_execution
     executor = "SettingExecution::#{params[:category].capitalize}".safe_constantize
     if executor.respond_to?(params[:command])
