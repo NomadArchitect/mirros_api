1524247c1322cba70f40c0323ea4c9650dcd5a24
Tobias Grasse <tg@glancr.de>
2018-10-01 10:23:10 +0200

    Improve Network superclass logic and method naming

--- app/lib/setting_execution/network.rb
+++ app/lib/setting_execution/network.rb
@@ -4,29 +4,34 @@ module SettingExecution
   class Network
 
     # TODO: Support other authentication methods as well
-    def self.join
+    def self.connect
       ssid = Setting.find_by_slug('network_ssid').value
       password = Setting.find_by_slug('network_password').value
+      raise ArgumentError, 'SSID and password must be set' unless ssid.present? && password.present?
 
-      success = os_subclass.join(ssid, password)
-      Rails.logger.error "Error setting SSID to #{ssid}" unless success
+      success = os_subclass.connect(ssid, password)
+      # TODO: Errors should be raised for API clients
+
+      Rails.logger.error "Error joining WiFi with SSID #{ssid}" unless success
       success
     end
 
     def self.list
       available_networks = os_subclass.list
-      Rails.logger.error "Could not retrieve WiFi list" if available_networks.empty?
+      Rails.logger.error 'Could not retrieve WiFi list' if available_networks.empty?
       available_networks
     end
 
-    private_class_method def self.os_subclass
+    def self.os_subclass
       if OS.linux?
         NetworkLinux
       elsif OS.mac?
         NetworkMac
-      # TODO: Maybe implement Windows someday.
+        # TODO: Maybe implement Windows someday.
       end
     end
+
+    private_class_method :os_subclass
 
   end
 end
