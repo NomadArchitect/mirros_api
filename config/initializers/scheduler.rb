ab8b1ed0e5c9797d8370a59fba36d16cbe70e8c9
Tobias Grasse <tg@glancr.de>
2018-10-07 12:22:46 +0200

    Initialize current ip and add scheduled job

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,4 +6,9 @@ require 'rufus-scheduler'
 # only schedule when not running from the Ruby on Rails console or from a rake task
 if Rails.const_defined? 'Server'
   MirrOSApi::DataRefresher.schedule_all
+
+  Rails.configuration.current_ip = System.current_ip
+  Rufus::Scheduler.s.every '1m' do
+    System.check_ip_change
+  end
 end
