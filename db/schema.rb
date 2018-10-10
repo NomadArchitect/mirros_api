b58a3109d03a74a359f143b5931e68856809e396
Tobias Grasse <tg@glancr.de>
2018-10-10 09:41:32 +0200

    Make group relation of widget optional
    Set `optional` parameter to true, remove NOT NULL constraint from table definition.

--- db/schema.rb
+++ db/schema.rb
@@ -159,7 +159,7 @@ ActiveRecord::Schema.define(version: 2018_07_31_183445) do
     t.string "slug", null: false
     t.string "icon"
     t.string "languages"
-    t.string "group_id", null: false
+    t.string "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_widgets_on_group_id"
