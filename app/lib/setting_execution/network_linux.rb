5d2c2a76568086ece7a22197a900da44554cc038
Tobias Grasse <tg@glancr.de>
2018-10-10 12:29:06 +0200

    Run the same command for all Linux distributions
    Added a class-level comment about required packages. Supporting multiple distributions is a semi-required feature at best, so it’s easier to require network-manager installation than just assuming that future Raspbian versions include specific network tools by default.

--- app/lib/setting_execution/network_linux.rb
+++ app/lib/setting_execution/network_linux.rb
@@ -1,16 +1,22 @@
 module SettingExecution
 
-  # Apply network-related settings.
+  # Apply network-related settings in Linux environments.
+  #
+  # Requires the packages network-manager (provides nmcli) and wireless-tools
+  # (provides iwlist) to be installed and executable. The AP-related methods
+  # assume that there is a valid nmcli connection named `glancrsetup`.
   class NetworkLinux < Network
 
     # TODO: Support other authentication methods as well
     def self.connect(ssid, password)
-      line = connect_command_for_distro
+      line = Terrapin::CommandLine.new('nmcli', 'd wifi connect :ssid password :password')
       line.run(ssid: ssid, password: password)
     end
 
     def self.list
-      line = list_command_for_distro
+      # TODO: Terrapin::CommandLine.new('nmcli -t --fields SSID d wifi list')
+      # would be prettier, but we require two interfaces to scan while in AP mode.
+      line = Terrapin::CommandLine.new('iwlist', 'wlan0 scan | grep ESSID | cut -d "\"" -f 2')
       line.run
     end
 
@@ -38,24 +44,5 @@ module SettingExecution
       result << dns_line.run
       result
     end
-
-    def self.connect_command_for_distro
-      distro = System.determine_linux_distro
-      {
-        'Ubuntu': Terrapin::CommandLine.new('nmcli', 'd wifi connect :ssid password :password'),
-        'Raspbian': Terrapin::CommandLine.new('wpa_passphrase', ':ssid :password | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null && sudo wpa_cli -i wlan0 reconfigure')
-      }[distro]
-    end
-    private_class_method :connect_command_for_distro
-
-    def self.list_command_for_distro
-      distro = System.determine_linux_distro
-      {
-        'Ubuntu': Terrapin::CommandLine.new('nmcli -t --fields SSID d wifi list'),
-        'Raspbian': Terrapin::CommandLine.new('iwlist', 'wlan0 scan | grep ESSID | cut -d "\"" -f 2')
-      }[distro]
-    end
-    private_class_method :list_command_for_distro
-
   end
 end
