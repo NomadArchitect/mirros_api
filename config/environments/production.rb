bc999f8d802992c2e76d1c8137ee513c196fca6b
Tobias Grasse <tg@glancr.de>
2018-10-18 16:38:37 +0200

    Raise default log level to :warn
    :info would bloat our logs until the frontend uses websockets for data refreshes. Until then, logging the whole request every second just generates huge log files which might err when sending debug reports.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,9 +47,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :info
+  # Raise default log level to warn to avoid log bloat from the frontend requests.
+  # TODO: Check if this can be changed during runtime (should be the case), see https://gitlab.com/glancr/mirros_api/issues/62
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
