eba53891d04c01b06d2d9ac16b04037a161656da
Tobias Grasse <tg@glancr.de>
2018-11-22 14:17:08 +0100

    Put newsfeed items in reverse chronological order
    Assumes that widgets want to display items in reverse chronological order.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/resources/group_schemas/newsfeed_resource.rb
+++ app/resources/group_schemas/newsfeed_resource.rb
@@ -3,6 +3,6 @@
     attributes :name, :url, :icon_url, :latest_entry, :items
 
     def items
-      @model.items.sort_by(&:published).as_json(except: %i[newsfeed_id])
+      @model.items.sort_by(&:published).reverse.as_json(except: %i[newsfeed_id])
     end
   end
