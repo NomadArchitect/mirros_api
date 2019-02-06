502f635de1eb8da974e8145d9392c053fc70e7e1
Tobias Grasse <tg@glancr.de>
2019-02-06 14:15:41 +0100

    Do not show widget title by default
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/migrate/20171101091044_create_widget_instances.rb
+++ db/migrate/20171101091044_create_widget_instances.rb
@@ -3,7 +3,7 @@ class CreateWidgetInstances < ActiveRecord::Migration[5.1]
     create_table :widget_instances do |t|
       t.string :widget_id
       t.string :title
-      t.boolean :showtitle, default: true # JSONAPI resources seems to hiccup on underscored field names, see https://github.com/cerebris/jsonapi-resources/pull/158
+      t.boolean :showtitle, default: false # JSONAPI resources seems to hiccup on underscored field names, see https://github.com/cerebris/jsonapi-resources/pull/158
       t.json :configuration, default: {}
       t.json :position
       t.timestamps
