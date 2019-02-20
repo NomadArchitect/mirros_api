2258e0f998360e9a4cf20237e296c6d5fe5dafe8
Tobias Grasse <tg@glancr.de>
2019-02-20 12:59:47 +0100

    Add after_destroy callback for frontend refresh
    Ensure frontend is reloaded after something has been deleted as well.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   after_save :set_refresh_flag
+  after_destroy :set_refresh_flag
 
   def set_refresh_flag
     session_file = 'tmp/session.yml'
