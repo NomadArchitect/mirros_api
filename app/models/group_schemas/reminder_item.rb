cfedda2a52d55200d64277f63317705df24be46a
Tobias Grasse <tg@glancr.de>
2019-02-21 15:03:57 +0100

    Add iso8601 date fix for reminder models as well
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/group_schemas/reminder_item.rb
+++ app/models/group_schemas/reminder_item.rb
@@ -1,6 +1,18 @@
 module GroupSchemas
   class ReminderItem < ApplicationRecord
     belongs_to :reminder_list
+
+    # Override ActiveModel method to ensure ISO8601 formatted dates. Required since jsonapi-resources calls
+    # JSON.generate instead of ActiveSupport methods, thus calling to_s on the date fields which outputs
+    # "yyyy-mm-dd hh:mm:ss UTC" which in turn breaks JS' Date parsing in all browsers except Chrome.
+    # see https://github.com/cerebris/jsonapi-resources/blob/d2db72b370b9150e3363e3cd406294e9cacfcc2f/lib/jsonapi/acts_as_resource_controller.rb#L241
+    def serializable_hash(options = nil)
+      base = super({except: %i[uid calendar_id dtstart dtend]}.merge(options || {}))
+      base.merge(
+        dtstart: dtstart.iso8601,
+        dtend: dtend.iso8601
+      )
+    end
   end
 end
 
--- app/resources/group_schemas/calendar_resource.rb
+++ app/resources/group_schemas/calendar_resource.rb
@@ -3,7 +3,6 @@ class CalendarResource < RecordableResource
   attributes :name, :description, :events
 
   def events
-    #
     @model.events.sort_by(&:dtstart).as_json
   end
 end
