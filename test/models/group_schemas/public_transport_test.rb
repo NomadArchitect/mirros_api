9f523c6d54946d49daf9aeb3722e9f7563227ee4
Tobias Grasse <mail@tobias-grasse.net>
2019-03-09 19:00:25 +0000

    Add missing model tests
    Seems like some models were generated without test files, or those were not committed
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/models/group_schemas/public_transport_test.rb
+++ test/models/group_schemas/public_transport_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class GroupSchemas::PublicTransportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
