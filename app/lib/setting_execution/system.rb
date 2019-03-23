f45bddd79e3a9039e977ba1f58fe7881405e19f0
Tobias Grasse <tg@glancr.de>
2019-03-24 00:38:05 +0100

    Improve dev env handling and formatting in System.timezone
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/setting_execution/system.rb
+++ app/lib/setting_execution/system.rb
@@ -1,21 +1,23 @@
 # frozen-string-literal: true
+
 require 'dbus'
 
 module SettingExecution
 
-  # Provides methods to apply settings in the display namespace.
+  # Provides methods to apply settings in the system namespace.
   class System
-    def self.timezone(tz)
+    def self.timezone(tz_identifier)
+      return if OS.mac? && Rails.env.development? # Bail in macOS dev env.
       raise NotImplementedError, 'Timezone control only implemented for Linux hosts' unless OS.linux?
 
       sysbus = DBus.system_bus
-      timedated_service = sysbus["org.freedesktop.timedate1"]
-      timedated_object = timedated_service["/org/freedesktop/timedate1"]
-      timedated_interface = timedated_object["org.freedesktop.timedate1"]
+      timedated_service = sysbus['org.freedesktop.timedate1']
+      timedated_object = timedated_service['/org/freedesktop/timedate1']
+      timedated_interface = timedated_object['org.freedesktop.timedate1']
       # Ref: https://www.freedesktop.org/wiki/Software/systemd/timedated/
       # tz needs to be a valid timezone from /usr/share/zoneinfo/zone.tab
       # bool: User interaction
-      timedated_interface.SetTimezone(tz, false)
+      timedated_interface.SetTimezone(tz_identifier, false)
       # TODO: See if this needs additional error handling, either here or in controller
     end
   end
