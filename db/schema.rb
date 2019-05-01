cb410af9d98eea09b6d6741fac0ef1fcbf93d6dc
Tobias Grasse <tg@glancr.de>
2019-05-01 13:15:30 +0200

    Update schema.rb
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_202357) do
+ActiveRecord::Schema.define(version: 2019_04_30_230138) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
@@ -30,7 +51,7 @@ ActiveRecord::Schema.define(version: 2019_02_23_202357) do
     t.datetime "dtend"
     t.boolean "all_day"
     t.string "summary"
-    t.string "description"
+    t.text "description"
     t.index ["calendar_id"], name: "index_group_schemas_calendar_events_on_calendar_id"
     t.index ["uid"], name: "index_group_schemas_calendar_events_on_uid"
   end
