4deafa2411e9db9e4dcfbcbb4f4c89b6027dabb2
Tobias Grasse <tg@glancr.de>
2019-06-24 14:48:38 +0200

    Add UID to newsfeed resource
    Enables filtering if a newsfeed resource provides multiple sub-resources.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/resources/group_schemas/newsfeed_resource.rb
+++ app/resources/group_schemas/newsfeed_resource.rb
@@ -1,8 +1,13 @@
   class NewsfeedResource < RecordableResource
     model_name 'GroupSchemas::Newsfeed'
     attributes :name, :url, :icon_url, :latest_entry, :items
+    attribute :uid
 
     def items
       @model.items.sort_by(&:published).reverse.as_json(except: %i[newsfeed_id])
+    end
+
+    def uid
+      @model.url
     end
   end
