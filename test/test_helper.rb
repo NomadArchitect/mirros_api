c338f43c5c62bd726e992d3862fbbcdf811bb25d
Tobias Grasse <tg@glancr.de>
2019-07-17 12:26:51 +0200

    Improve integration tests
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,22 +1,24 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  def set_jsonapi_headers
-    @headers = {
-      'ACCEPT' => 'application/vnd.api+json',
-      'CONTENT_TYPE' => 'application/vnd.api+json'
-    }
+    def jsonapi_headers
+      {
+        'ACCEPT': 'application/vnd.api+json',
+        'CONTENT-TYPE': 'application/vnd.api+json'
+      }
+    end
+
+    def jsonapi_wrap(type, resource)
+      {
+        data: resource.as_json.merge(type: type)
+      }
+    end
+
+    # Add more helper methods to be used by all tests here...
   end
-
-  def jsonapi_wrap(type, resource)
-    {
-      data: resource.as_json.merge(type: type)
-    }
-  end
-
-  # Add more helper methods to be used by all tests here...
 end
