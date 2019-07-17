3c3b8a55f8374259f7eda204dd34ce5082da9088
Tobias Grasse <tg@glancr.de>
2019-07-17 12:08:46 +0200

    Update direct dependencies
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- default-gems.rb
+++ default-gems.rb
@@ -5,7 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 source 'https://rubygems.org' do
   gem 'rails', '~> 5.2.3'
-  gem 'puma', '~> 3.11'
+  gem 'puma', '~> 4.0.1'
   gem 'bootsnap', '>= 1.1.0', require: false
   gem 'rack-cors'
   group :development, :test do
@@ -18,20 +18,22 @@ source 'https://rubygems.org' do
     # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
     gem 'spring'
     gem 'spring-watcher-listen', '~> 2.0.0'
-
-    gem 'sqlite3', '~> 1.3.6'
     gem 'better_errors', '~> 2.5.0'
     gem 'binding_of_caller', '~> 0.8.0'
     gem 'git', '~> 1.5.0'
     # Data Visualization
-    gem 'rails-erd', '~> 1.5.0'
+    gem 'rails-erd', '~> 1.6.0'
+  end
+
+  group :test do
+    gem 'sqlite3', '~> 1.4'
   end
 
   ### mirr.OS gems ###
   gem 'mysql2', '~> 0.5.2'
   gem 'bundler', '>= 1.17.1' # extension management
   gem 'httparty', '~> 0.17.0'
-  gem 'jsonapi-resources', '~> 0.9.6'
+  gem 'jsonapi-resources', '~> 0.9.10'
   gem 'rufus-scheduler', '~> 3.6.0'
   gem 'friendly_id', '~> 5.2.5'
   gem 'os', '~> 1.0.0'
