7edddf6271379f0249cd5b399d24e51c3e071942
Tobias Grasse <tg@glancr.de>
2019-07-24 14:46:33 +0200

    Commit `active` field in widget migration
    Was missing from c00f6a25ed680370c568345c08471c69006d6598
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/migrate/20171101094603_create_widgets.rb
+++ db/migrate/20171101094603_create_widgets.rb
@@ -2,6 +2,7 @@ class CreateWidgets < ActiveRecord::Migration[5.1]
   def change
     create_table :widgets do |t|
       t.string :name, null: false
+      t.boolean :active, default: true
       t.json :title
       t.string :compatibility
       t.json :description
