82f2b44564fd6061c280d8284114ab66fbc09bd8
Tobias Grasse <tg@glancr.de>
2019-07-26 16:39:33 +0200

    Add subclass typing to Upload model
    Allows extensions to have their own namespaced uploads so they don’t have to deal with filtering the global uploads list. Nice and easy.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,29 +1,35 @@
 class UploadsController < ApplicationController
   # include JSONAPI::ActsAsResourceController
+  before_action :set_upload_type
+
   def index
-    render json: Upload.all, methods: :file_url
+    render json: @type.all, methods: :file_url
   end
 
   def create
-    render json: Upload.create!(upload_params), methods: :file_url, status: 201
+    render json: @type.create!(upload_params), methods: :file_url, status: 201
   end
 
   def show
-    render json: Upload.find(params[:id]), methods: :file_url
+    render json: @type.find(params[:id]), methods: :file_url
   end
 
   def update
-    render json: Upload.find(params[:id]).update(upload_params), methods: :file_url
+    render json: @type.find(params[:id]).update(upload_params), methods: :file_url
   end
 
   def destroy
-    Upload.find(params[:id]).purge_and_destroy
+    @type.find(params[:id]).purge_and_destroy
   end
 
   private
 
   def upload_params
-    params.require(:upload).permit(:file)
+    params.require(:upload).permit(:file).merge(type: @type)
+  end
+
+  def set_upload_type
+    @type = controller_path.classify.safe_constantize ||= Upload
   end
 
 end
