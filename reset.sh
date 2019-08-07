b4e55ee0a6780ec6195d7d380c8f57212e28980b
Tobias Grasse <tg@glancr.de>
2019-08-07 09:04:26 +0200

    Fix reset script
    Reset was deleting the whole directory, which might lead to issues with bind-mounts inside the snap – in any case, the copy command went awry due to missing target folders.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- reset.sh
+++ reset.sh
@@ -2,9 +2,9 @@
 ENV=$1
 
 if [[ ${ENV} == 'production' && -n ${SNAP} ]]; then
-    rm -rf $SNAP_DATA/rails/.bundle \
-           $SNAP_DATA/rails/vendor \
-           $SNAP_DATA/rails/db \
+    rm -rf $SNAP_DATA/rails/.bundle/* \
+           $SNAP_DATA/rails/vendor/* \
+           $SNAP_DATA/rails/db/* \
            $SNAP_DATA/rails/Gemfile \
            $SNAP_DATA/rails/Gemfile.lock
 
@@ -13,7 +13,7 @@ if [[ ${ENV} == 'production' && -n ${SNAP} ]]; then
     cp -r $SNAP/api/db_installed/* $SNAP_DATA/rails/db
     cat $SNAP/api/Gemfile_installed > $SNAP_DATA/rails/Gemfile
     cat $SNAP/api/Gemfile.lock_installed > $SNAP_DATA/rails/Gemfile.lock
-    bundle clean
+    bundle clean --force
 else
     git checkout -- Gemfile
     git checkout -- Gemfile.lock
