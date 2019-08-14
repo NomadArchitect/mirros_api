4706b77e9c0bec97a26dea6c00245a7dbb0ba015
Tobias Grasse <tg@glancr.de>
2019-08-14 11:18:15 +0200

    Code formatting and conventions
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/group_schemas/public_transport.rb
+++ app/models/group_schemas/public_transport.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 module GroupSchemas
+  # Base model for public transport data schema.
   class PublicTransport < ApplicationRecord
-    validates_presence_of :type
+    validates :type, presence: true
     has_one :record_link, as: :recordable, dependent: :destroy
-    has_many :departures, class_name: 'PublicTransportDeparture', dependent: :delete_all, autosave: true
+    has_many :departures,
+             class_name: 'PublicTransportDeparture',
+             dependent: :delete_all,
+             autosave: true
   end
 end
