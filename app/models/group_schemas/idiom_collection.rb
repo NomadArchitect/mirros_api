40f682bdb685baa0dc83b825bf17e6cdc9fc098f
Tobias Grasse <tg@glancr.de>
2019-08-21 15:40:04 +0200

    Fix presence validation offenses
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/group_schemas/idiom_collection.rb
+++ app/models/group_schemas/idiom_collection.rb
@@ -1,6 +1,6 @@
 module GroupSchemas
   class IdiomCollection < ApplicationRecord
-    validates_presence_of :type
+    validates :type, presence: true
     has_one :record_link, as: :recordable, dependent: :destroy
     has_many :items, class_name: 'IdiomCollectionItem', dependent: :delete_all, autosave: true
   end
