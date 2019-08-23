795efd3565735e56b8fce9c769580712065f0fd9
Tobias Grasse <tg@glancr.de>
2019-08-23 09:43:58 +0200

    Switch System.timezone from DBus to timedatectl cli
    See https://bugs.launchpad.net/snappy/+bug/1650688
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/setting_execution/system.rb
+++ app/lib/setting_execution/system.rb
@@ -9,19 +9,24 @@ module SettingExecution
     def self.timezone(tz_identifier)
       if OS.mac?
         # Bail in macOS dev env or if invoked during rake task
-        return if Rails.env.development? || Rails.const_defined?('Server')
+        return if Rails.env.development? || !Rails.const_defined?('Server')
 
         raise NotImplementedError, 'Timezone control only implemented for Linux hosts'
       end
 
-      sysbus = DBus.system_bus
-      timedated_service = sysbus['org.freedesktop.timedate1']
-      timedated_object = timedated_service['/org/freedesktop/timedate1']
-      timedated_interface = timedated_object['org.freedesktop.timedate1']
+      # sysbus = DBus.system_bus
+      # timedated_service = sysbus['org.freedesktop.timedate1']
+      # timedated_object = timedated_service['/org/freedesktop/timedate1']
+      # timedated_interface = timedated_object['org.freedesktop.timedate1']
+
+      line = Terrapin::CommandLine.new('timedatectl', 'set-timezone :timezone')
+      line.run(timezone: tz_identifier)
       # Ref: https://www.freedesktop.org/wiki/Software/systemd/timedated/
       # tz needs to be a valid timezone from /usr/share/zoneinfo/zone.tab
       # bool: User interaction
-      timedated_interface.SetTimezone(tz_identifier, false)
+
+      # timedated_interface.SetTimezone(tz_identifier, false)
+
       #::System.toggle_timesyncd_ntp(true)
       # TODO: See if this needs additional error handling, either here or in controller
     end
