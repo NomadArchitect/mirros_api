34b8c24e3539556f9c877f65a009f8e797dcb898
Tobias Grasse <tg@glancr.de>
2019-09-05 09:10:52 +0200

    Override AS::DiskController to enable caching headers
    To avoid constant reloading of local file uploads, this adds proper cache headers to served assets.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

---
+++ app/overrides/controllers/active_storage/disk_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+load ActiveStorage::Engine.root.join('app/controllers/active_storage/disk_controller.rb')
+
+ActiveStorage::DiskController.class_eval do
+  def show
+    if (key = decode_verified_key)
+      expires_in 1.year, public: true
+      serve_file disk_service.path_for(key[:key]), content_type: key[:content_type], disposition: key[:disposition]
+    else
+      head :not_found
+    end
+  end
+end
