99ba8187a3a905856cbf7f557169a8c9070b81b9
Tobias Grasse <tg@glancr.de>
2019-09-05 13:00:08 +0200

    Fix file_url generation for non-image uploads
    rails_representation_url is only for variants, which we don’t generate for non-image files.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,16 +4,17 @@ class Upload < ApplicationRecord
   def file_url
     return unless file.attached?
 
-    obj = if file.image?
-            file.variant(resize: '1920x1920').processed
-          else
-            file
-          end
-    Rails.application.routes.url_helpers.rails_representation_url(
-      obj,
-      host: ActiveStorage::Current.host
-    )
-
+    if file.image?
+      Rails.application.routes.url_helpers.rails_representation_url(
+        file.variant(resize: '1920x1920').processed,
+        host: ActiveStorage::Current.host
+      )
+    else
+      Rails.application.routes.url_helpers.rails_blob_url(
+        file,
+        host: ActiveStorage::Current.host
+      )
+    end
   end
 
   def purge_and_destroy
