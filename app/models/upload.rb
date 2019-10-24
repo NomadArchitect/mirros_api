e0c014cc0979a79fd8481a6165192ac824ac5629
Tobias Grasse <tg@glancr.de>
2019-10-24 13:41:06 +0200

    Do not attempt image processing for SVGs
    Pointless and throws errors.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,10 +1,12 @@
 class Upload < ApplicationRecord
   has_one_attached :file, dependent: :destroy
 
+  delegate :content_type, to: :file
+
   def file_url
     return unless file.attached?
 
-    if file.image?
+    if file.image? && !file.content_type.include?('svg')
       Rails.application.routes.url_helpers.rails_representation_url(
         file.variant(resize: '1920x1920').processed,
         host: ActiveStorage::Current.host
