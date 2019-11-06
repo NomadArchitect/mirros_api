8ec69636c47d8f0cdaba743cc4135d62fe5f21c2
Tobias Grasse <tg@glancr.de>
2019-11-06 10:39:26 +0100

    Only run NM sync initializer in server mode
    Would cause race conditions when run during rake tasks like db:setup (where the tables for Setting and NmNetwork do not exist yet)
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/nm_connections_sync.rb
+++ config/initializers/nm_connections_sync.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # FIXME: Temporary workaround until https://bugs.launchpad.net/snapd/+bug/1851480 is fixed
-if OS.linux?
+if OS.linux? && Rails.const_defined?('Server')
   # Synchronize NM connections to new NmNetwork model if required.
   networks = %w[glancrsetup glancrlan]
   ssid = Setting.find_by(slug: 'network_ssid')
