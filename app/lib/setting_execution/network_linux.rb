22b7bc09c9dcbaa4851b0d08472d1db63e9c7a80
Tobias Grasse <tg@glancr.de>
2019-11-12 12:15:12 +0100

    Add conditional to toggle_lan
    If the LAN connection was already in/active, toggling it off/on would result in errors.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/setting_execution/network_linux.rb
+++ app/lib/setting_execution/network_linux.rb
@@ -58,11 +58,13 @@ module SettingExecution
     private_class_method :normalize_signal_strength
 
     def self.toggle_lan(state)
+      ci = Commands.instance
+      cn = 'glancrlan'
       case state
       when 'on'
-        Commands.instance.activate_connection('glancrlan')
+        ci.activate_connection(cn) unless ci.connection_active?(cn)
       when 'off'
-        Commands.instance.deactivate_connection('glancrlan')
+        ci.deactivate_connection(cn) if ci.connection_active?(cn)
       else
         raise ArgumentError,
               "Could not toggle glancrlan to invalid state: #{state}"
