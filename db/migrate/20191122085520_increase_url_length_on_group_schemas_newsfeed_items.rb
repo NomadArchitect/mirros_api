94c78c1a4c893d80394369fea9c7a3e620bad636
Tobias Grasse <tg@glancr.de>
2019-11-22 10:45:37 +0100

    Increase newsfeed_items url field length
    Aggregators like Google News might produce URLs that have > 500 characters, which is way over the default VARCHAR limit of 255 characters.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

---
+++ db/migrate/20191122085520_increase_url_length_on_group_schemas_newsfeed_items.rb
@@ -0,0 +1,6 @@
+class IncreaseUrlLengthOnGroupSchemasNewsfeedItems < ActiveRecord::Migration[5.2]
+  def change
+
+    change_column :group_schemas_newsfeed_items, :url, :string, limit: 700
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_122635) do
+ActiveRecord::Schema.define(version: 2019_11_22_085520) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_122635) do
     t.string "newsfeed_id"
     t.string "title"
     t.text "content"
-    t.string "url"
+    t.string "url", limit: 700
     t.datetime "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
