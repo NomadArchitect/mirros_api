ba52ed209c649ab8e095be0838e93142593fe300
Tobias Grasse <tg@glancr.de>
2019-12-03 15:03:29 +0100

    Improve StateCache
    Adds generic callback on attr_writer method calls to push the latest state to clients. Have to check performance penalty of a global TracePoint though.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/state_cache.rb
+++ app/lib/state_cache.rb
@@ -6,12 +6,25 @@ class StateCache
   include Singleton
 
   attr_accessor :resetting, :connection_attempt, :setup_complete,
-                :configured_at_boot, :online, :connectivity,
-                :network_status
+                :configured_at_boot, :online, :primary_connection,
+                :connectivity, :network_status
   validates :resetting, :connection_attempt, inclusion: [true, false]
 
   class << self
     delegate_missing_to :instance
+  end
+
+  # FIXME: Maybe there's a nicer way to ensure instant Websocket updates on
+  # every StateCache write access?
+
+  # Add a generic handler that fires a websocket update every time the
+  # StateCache is updated. Other methods with metaprogramming are fragile for
+  # methods with parameters. Regex excludes initializer methods (would create
+  # infinite loops) and attr_reader methods to avoid spamming clients.
+  TracePoint.trace(:call) do |tp|
+    if tp.defined_class.eql?(StateCache) && !tp.method_id.match?(/^init|=$/)
+      ::System.push_status_update
+    end
   end
 
   def initialize
@@ -24,27 +37,35 @@ class StateCache
     # Remove once https://gitlab.com/glancr/mirros_api/issues/87 lands
     @configured_at_boot = @setup_complete
     @online = System.online?
-    @connectivity = initial_connectivity
+    @connectivity = init_connectivity
     @network_status = SettingExecution::Network.wifi_signal_status
 
     # private
-    primary_path = if OS.linux?
-                     NetworkManager::Commands.instance.primary_connection
-                   else
-                     '/'
-                   end
-    @primary_connection = update_primary_connection(primary_path)
+    @primary_connection = init_primary_connection
     @networks = NmNetwork.all.map(&:public_info)
   end
-
   def refresh_networks
     @networks = NmNetwork.all.map(&:public_info)
   end
 
+  # @param [Object] ac_path
+  # @return [nil]
   def update_primary_connection(ac_path)
-    @primary_connection = NmNetwork.find_by(
-      active_connection_path: ac_path
-    )&.public_info
+    if ac_path.eql?('/')
+      @primary_connection = nil
+    else
+      # Use a delayed job to ensure the connection is already persisted with
+      # its current active path.
+      Rufus::Scheduler.singleton.in '10s' do
+        @primary_connection = NmNetwork.find_by(
+          active_connection_path: ac_path
+        )&.public_info
+      end
+    end
+  end
+
+  def update_online_status(nm_state)
+    @online = System.state_is_online?(nm_state)
   end
 
   def self.as_json
@@ -57,11 +78,24 @@ class StateCache
 
   private
 
-  def initial_connectivity
+  def init_connectivity
     if OS.linux?
       NetworkManager::Commands.instance.connectivity
     else
       NetworkManager::Constants::NmConnectivityState::UNKNOWN
+    end
+  end
+
+  def init_primary_connection
+    primary_path = if OS.linux?
+                     NetworkManager::Commands.instance.primary_connection
+                   else
+                     '/'
+                   end
+    if primary_path.eql?('/')
+      nil
+    else
+      NmNetwork.find_by(active_connection_path: primary_path)&.public_info
     end
   end
 end
