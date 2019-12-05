5cc3d410b0ed8f154ae471460a3703892fc9ab45
Tobias Grasse <tg@glancr.de>
2019-12-05 13:24:05 +0100

    [Fix] Force manual connectivity refresh every 60s
    As NetworkManager doesn’t seem to publish connectivity changes reliably over DBus, this forces an update every minute to ensure clients get the actual state eventually.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -22,6 +22,15 @@ if Rails.const_defined? 'Server'
     listeners.listen
 
     at_exit { listeners.quit }
+
+    # As of N-M 1.10, the connectivity status doesn't seem to be pushed reliably
+    # over DBus. This ensures the StateCache is updated at least every minute.
+    # TODO: Revisit once we can use 1.16 or later on Core.
+    if NetworkManager::Commands.instance.connectivity_check_available?
+      s.every '60s', tag: 'network-connectivity-check', overlap: false do
+        StateCache.connectivity = NetworkManager::Commands.instance.connectivity
+      end
+    end
   else
     # On other hosts, we schedule network status check jobs.
     s.every '30s', tag: 'network-status-check', overlap: false do
