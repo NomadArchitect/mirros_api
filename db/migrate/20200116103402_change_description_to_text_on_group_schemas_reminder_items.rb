31a014fa310ea2e8ac44c422dd91f45d113c30e2
Tobias Grasse <tg@glancr.de>
2020-01-16 22:31:07 +0100

    [Sources] Change ReminderItems description field to text
    Signed-off-by: Tobias Grasse <tg@glancr.de>

---
+++ db/migrate/20200116103402_change_description_to_text_on_group_schemas_reminder_items.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptionToTextOnGroupSchemasReminderItems < ActiveRecord::Migration[5.2]
+  def change
+    change_column :group_schemas_reminder_items, :description, :text
+  end
+end
