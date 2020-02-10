f71738414e67c0e6409180085bff70bee757bf14
Tobias Grasse <tg@glancr.de>
2020-02-10 20:43:00 +0100

    Add group schema current_weather
    Signed-off-by: Tobias Grasse <tg@glancr.de>

---
+++ db/migrate/20200210172740_create_group_schemas_current_weathers.rb
@@ -0,0 +1,11 @@
+class CreateGroupSchemasCurrentWeathers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :group_schemas_current_weathers, id: false do |t|
+      t.string :id, primary_key: true
+      t.string :type
+      t.string :station_name
+      t.string :location
+      t.timestamps
+    end
+  end
+end
