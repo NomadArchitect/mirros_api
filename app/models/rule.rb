bf24e3c5cc9d105e6cb85e77f982eda54bcd3c6c
Tobias Grasse <tg@glancr.de>
2020-02-29 12:28:19 +0100

    Add validation logic to Rule model
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -2,4 +2,25 @@
 
 # Model that holds a single rule which is as part of a board's rule set.
 class Rule < ApplicationRecord
+  include ActiveModel::Validations
+
+  attr_reader :op
+
+  belongs_to :board
+  belongs_to :source_instance, optional: true
+
+  before_validation :setup
+  validates_each :value do |record, attr, value|
+    record.op.parse(value)
+  rescue StandardError => e
+    record.errors.add attr, "Value #{value} incompatible with #{@op} operator: #{e.message}"
+  end
+
+
+  def setup
+    @provider_class = "RuleManager::#{provider.camelize}RulesProvider".safe_constantize
+    raise ArgumentError, "Failed to instantiate RuleManager::#{provider.camelize}RulesProvider" if @provider_class.nil?
+
+    @op = @provider_class.rules.dig(field.to_sym, :operators, operator.to_sym)
+  end
 end
