e9586318eddeac0a35b81ef47b5155ae82c16808
Tobias Grasse <tg@glancr.de>
2020-02-29 13:50:29 +0100

    Update schema
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_190510) do
+ActiveRecord::Schema.define(version: 2020_02_29_115830) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
