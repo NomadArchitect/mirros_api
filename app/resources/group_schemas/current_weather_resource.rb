60d3f62f3b920f904c344852d9d0fc65fe7a2651
Marco Roth <marco.roth@intergga.ch>
2020-03-28 00:55:12 +0100

    add CurrentWeatherResource GroupSchema

---
+++ app/resources/group_schemas/current_weather_resource.rb
@@ -0,0 +1,9 @@
+class CurrentWeatherResource < RecordableResource
+  model_name 'GroupSchemas::CurrentWeather'
+  attributes :station_name, :entries
+  key_type :string
+
+  def entries
+    @model.entries.as_json(except: %i[current_weather_id uid])
+  end
+end
