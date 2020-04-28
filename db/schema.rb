50e918282d25b60d1a026bc2d7144dcb740c6476
Tobias Grasse <tg@glancr.de>
2020-04-28 12:42:34 +0200

    fix: Migrate InstanceAssociations to current_weather schema
    Existing associations for owm_current_weather widget instances should be updated. This is a one-off migration, so we don’t bloat the db_updates task.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_235358) do
+ActiveRecord::Schema.define(version: 2020_04_28_102630) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
