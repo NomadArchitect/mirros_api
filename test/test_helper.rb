f03ae3ec20558ce65f13b8ab2b8853ec79ce51fa
Tobias Grasse <tg@glancr.de>
2020-06-16 14:06:26 +0200

    test: Add assert_invalid helper method
    Asserts that a model is marked as invalid and has the given error message on the given attribute.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,6 +19,13 @@ module ActiveSupport
       }
     end
 
-    # Add more helper methods to be used by all tests here...
+    # Taken from https://stackoverflow.com/a/42618473/2915244
+    def assert_invalid(record, options)
+      assert_predicate record, :invalid?
+
+      options.each do |attribute, message|
+        assert record.errors.added?(attribute, message), "Expected #{attribute} to have the following error: #{message}"
+      end
+    end
   end
 end
