965f754f24b12b8b95a1d00a914b38748222bf64
Tobias Grasse <tg@glancr.de>
2020-06-17 11:46:52 +0200

    test: Disable dummy PublicTransportTest
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/models/group_schemas/public_transport_test.rb
+++ test/models/group_schemas/public_transport_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class GroupSchemas::PublicTransportTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+module GroupSchemas
+  class PublicTransportTest < ActiveSupport::TestCase
+
   end
 end
