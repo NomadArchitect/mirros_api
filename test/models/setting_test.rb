e2336967ab20463686989ade01909d19b05849b4
Tobias Grasse <tg@glancr.de>
2020-06-19 12:59:48 +0200

    refactor: Add helpers for license-related setup
    Extracts logic for setting up a valid/invalid product key in the database, so that tests can ensure the required conditions are set for the actual setting test.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -3,11 +3,25 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
+  VALID_LICENSE = '173AB351-D746-48CD-ACD2-764BE02AF52F'
+  INVALID_LICENSE = 'fnord'
+
   fixtures :settings # TODO: Remove once test_helper is active
 
-  test 'prevents changing a premium setting without a valid license' do
+  def save_valid_product_key
     license = settings('personal_productkey')
-    license.update(value: '')
+    license.update(value: VALID_LICENSE)
+    license
+  end
+
+  def save_invalid_product_key
+    license = settings('personal_productkey')
+    license.update(value: INVALID_LICENSE)
+    license
+  end
+
+  test 'prevents changing a premium setting without a valid license' do
+    save_invalid_product_key
 
     premium_setting = settings('system_showerrornotifications')
     premium_setting.update(value: 'off')
@@ -18,33 +32,24 @@ class SettingTest < ActiveSupport::TestCase
     )
   end
 
-  test 'Rejects an invalid product key' do
-    setting = settings('personal_productkey')
-    setting.update(value: 'fnord')
 
+  test 'Rejects an invalid product key' do
+    setting = save_invalid_product_key
     assert_invalid(setting, value: "#{setting.value} is not a valid product key.")
   end
 
   test 'Saves a valid product key' do
-    setting = settings('personal_productkey')
-    setting.update(value: '173AB351-D746-48CD-ACD2-764BE02AF52F')
-
+    setting = save_valid_product_key
     assert setting.valid?
   end
 
   test 'Removing a product key resets premium features' do
-    license = settings('personal_productkey')
-    license.value = '173AB351-D746-48CD-ACD2-764BE02AF52F'
-    license.save
-
+    license = save_valid_product_key
     premium_setting = settings('system_showerrornotifications')
-    premium_setting.save
-
     premium_setting.update(value: 'off')
-
     license.update(value: '')
-    assert license.valid?
 
+    assert license.valid?
     assert Setting.find(premium_setting.id).value.eql? 'on'
   end
 end
