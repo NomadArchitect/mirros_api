b594ae2329023d1c6e388c1dce03ef2325d5fe5c
Tobias Grasse <tg@glancr.de>
2020-07-04 14:07:39 +0200

    feat: Flatten Background relation on Board JSON res
    We don’t want to expose a full Background JSON model, so this eases manipulation since we just need to modify the background ID:
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/resources/board_resource.rb
+++ app/resources/board_resource.rb
@@ -1,5 +1,5 @@
 class BoardResource < JSONAPI::Resource
-  attributes :title
+  attributes :title, :background_id, :background_url
 
   has_many :widget_instances,
            always_include_linkage_data: true,
@@ -7,4 +7,20 @@ class BoardResource < JSONAPI::Resource
   has_many :rules,
            always_include_linkage_data: true,
            exclude_links: [:self]
+
+  def background_id
+    @model.background&.id
+  end
+
+  def background_url
+    @model.background&.file_url
+  end
+
+  def background_id=(background_id)
+    if background_id.blank?
+      @model.background&.boards&.delete(id)
+    else
+      @model.background = Background.find(background_id)
+    end
+  end
 end
