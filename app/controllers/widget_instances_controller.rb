d0ecbd3b0d649c740ca4ce0477ac3ace47deaa77
Tobias Grasse <tg@glancr.de>
2020-07-04 14:10:27 +0200

    fix: Set ActiveStorage host for controllers
    Background-related requests need this, or will fail with “no host to route to!” when generating the `background_url`. WidgetInstancesController requires this as its the one called when adding a widget to a board.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/controllers/widget_instances_controller.rb
+++ app/controllers/widget_instances_controller.rb
@@ -2,4 +2,10 @@
 
 class WidgetInstancesController < ApplicationController
   include JSONAPI::ActsAsResourceController
+
+  # Enables background image URL generation. @see UploadsController
+  # FIXME: Evaluate if this should be on ApplicationController or even static.
+  before_action do
+    ActiveStorage::Current.host = request.base_url
+  end
 end
