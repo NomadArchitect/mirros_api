fd3d1c7f6f27c9ab27de5c9b5ef6a4e0aa25bed2
Tobias Grasse <tg@glancr.de>
2020-08-26 14:33:40 +0200

    test: Add basic tests for file upload
    Add first integration test for upload functionality.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/controllers/uploads_controller_test.rb
+++ test/controllers/uploads_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UploadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can upload files" do
+    assert Upload.count.eql? 0
+
+    post '/uploads', params: { upload: { file: fixture_file_upload('files/glancr_logo.png', 'image/png') } }
+
+    assert Upload.count.eql? 1
+    assert_response :created
+    assert @response.parsed_body['content_type'].eql? 'image/png'
+  end
+
+  test "uploaded files are cacheable" do
+    post '/uploads', params: { upload: { file: fixture_file_upload('files/glancr_logo.png', 'image/png') } }
+
+    get @response.parsed_body['file_url']
+    follow_redirect!
+
+    assert @response.cache_control[:public].eql? true
+    assert @response.cache_control[:max_age].to_i > 0
+  end
 end
