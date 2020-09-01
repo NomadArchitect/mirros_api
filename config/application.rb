c03119685b4c2282c54927f8677a068f2a06f3b3
Tobias Grasse <tg@glancr.de>
2020-09-01 09:12:13 +0200

    refactor: Make bundler groups explicit during boot
    We won’t be introducing a third group anytime soon, and this removes the need to manually require Installable during application startup.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/application.rb
+++ config/application.rb
@@ -17,12 +17,11 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
-require_relative '../app/models/concerns/installable'
 require_relative 'versions.rb'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups, *Installable::EXTENSION_TYPES)
+Bundler.require(*Rails.groups, *%w[widget source].freeze)
 
 module MirrOSApi
   class Application < Rails::Application
