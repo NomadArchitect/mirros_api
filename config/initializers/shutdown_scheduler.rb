6fa849ff43c0d44df73cbefc3ab626ef81f43a5e
Tobias Grasse <tg@glancr.de>
2020-09-21 12:06:39 +0200

    fix: Only run shutdown schedule in server mode
    Would block rake tasks like db:create if there is no database to read from.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/shutdown_scheduler.rb
+++ config/initializers/shutdown_scheduler.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-shutdown_time = SettingsCache.s[:system_scheduleshutdown]
-if shutdown_time.present?
-  SettingExecution::System.schedule_shutdown shutdown_time
-  Rails.logger.info "Scheduled shutdown at #{shutdown_time.to_time}"
+if Rails.const_defined? 'Server'
+  shutdown_time = SettingsCache.s[:system_scheduleshutdown]
+  if shutdown_time.present?
+    SettingExecution::System.schedule_shutdown shutdown_time
+    Rails.logger.info "Scheduled shutdown at #{shutdown_time.to_time}"
+  end
 end
