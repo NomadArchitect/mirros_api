e4e25aa488846a05c6e4f45723ca3d2466be31f4
Tobias Grasse <tg@glancr.de>
2020-09-25 11:11:30 +0200

    refactor: Rename instance variable
    @op is too vague. @operator_class also better conveys that we’re dealing with a class, reducing confusion with the Rule model’s `operator` attribute.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -4,28 +4,28 @@
 class Rule < ApplicationRecord
   include ActiveModel::Validations
 
-  attr_reader :op
+  attr_reader :operator_class
 
   belongs_to :board
   belongs_to :source_instance, optional: true
 
   before_validation :setup
   validates_each :value do |record, attr, value|
-    record.op.parse(value)
+    record.operator_class.parse(value)
   rescue StandardError => e
-    record.errors.add attr, "Value #{value} incompatible with #{@op} operator: #{e.message}"
+    record.errors.add attr, "Value #{value} incompatible with #{@operator_class} operator: #{e.message}"
   end
 
   def evaluate
     setup
     # TODO: Remove parse call if we implement typecasting before save
-    @op.evaluate @provider_class.method(field.underscore).call, @op.parse(value)
+    @operator_class.evaluate @provider_class.method(field.underscore).call, @operator_class.parse(value)
   end
 
   def setup
     @provider_class = "RuleManager::#{provider.camelize}RulesProvider".safe_constantize
     raise ArgumentError, "Failed to instantiate RuleManager::#{provider.camelize}RulesProvider" if @provider_class.nil?
 
-    @op = @provider_class.rules.dig(field.to_sym, :operators, operator.to_sym)
+    @operator_class = @provider_class.rules.dig(field.to_sym, :operators, operator.to_sym)
   end
 end
