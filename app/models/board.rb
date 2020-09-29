640b5ddacb233749b636d4a79c4e9648eb923dfd
Tobias Grasse <tg@glancr.de>
2020-09-29 12:13:31 +0200

    feat: Abort destroy if called on default board
    We do not want the default board to be deleted, as it’s the fallback if no rule applies.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,13 +2,20 @@
 
 # Model for a screen configuration.
 class Board < ApplicationRecord
-  before_destroy :abort_if_active
+  before_destroy :abort_if_default, :abort_if_active
   has_many :widget_instances, dependent: :destroy
   has_many :rules, dependent: :destroy
   belongs_to :background, # background may be re-used across boards.
              foreign_key: :uploads_id, # use a single table for uploads.
              inverse_of: :boards,
              optional: true # background image is optional.
+
+  def abort_if_default
+    return unless id.eql?(Board.first.id)
+
+    errors.add :base, I18n.t('board.errors.messages.default_board_deletion')
+    throw(:abort)
+  end
 
   def abort_if_active
     return unless id.eql?(SettingsCache.s[:system_activeboard].to_i)
