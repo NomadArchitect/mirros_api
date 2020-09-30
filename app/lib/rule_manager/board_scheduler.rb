f594505f7b84a27836094a7e5fb1f9fc4078d0f5
Tobias Grasse <tg@glancr.de>
2020-09-30 08:49:44 +0200

    feat: Fall back to default board if no rule applies
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/rule_manager/board_scheduler.rb
+++ app/lib/rule_manager/board_scheduler.rb
@@ -13,20 +13,25 @@ module RuleManager
       return if job_running? RULE_EVALUATION_TAG
 
       Rufus::Scheduler.singleton.cron '* * * * *', tag: RULE_EVALUATION_TAG do
-        Board.all.each do |board|
+        rule_applied = false
+          Board.all.each do |board|
           # TODO: Extend logic for rule sets in each board.
 
           # Date-based rules take precedence over recurring rules.
           if board.rules.where(provider: 'system', field: 'dateAndTime').any?(&:evaluate)
             Setting.find_by(slug: :system_activeboard).update(value: board.id)
+            rule_applied = true
             break
           end
           # TODO: this only gets time-based rules for the board and runs them in sequential order.
           if board.rules.where(provider: 'system', field: 'timeOfDay').any?(&:evaluate)
             Setting.find_by(slug: :system_activeboard).update(value: board.id)
+            rule_applied = true
             break
           end
         end
+
+        Setting.find_by(slug: :system_activeboard).update(value: Board.first.id) unless rule_applied
       end
       Rails.logger.info "scheduled job #{RULE_EVALUATION_TAG}"
     end
