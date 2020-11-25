5ca8c2f188c6c28cae38705b9b11980d325d43a2
Tobias Grasse <tg@glancr.de>
2020-11-25 09:05:19 +0100

    feat: Store client display information in SystemState
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/channels/status_channel.rb
+++ app/channels/status_channel.rb
@@ -8,4 +8,18 @@ class StatusChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  # @param [Object] data A hash containing fields `orientation`, `width` and `height`.
+  # @return [ActiveModel]
+  def client_display(data)
+    SystemState
+      .find_or_initialize_by(variable: 'client_display')
+      .update(
+        value: {
+          orientation: data['orientation'] || 'portrait',
+          width: data['width'] || 1080,
+          height: data['height'] || 1920
+        }
+      )
+  end
 end
