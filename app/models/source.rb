78ccaa254f6099659d7c31c5afad4b3a62aad267
Tobias Grasse <tg@glancr.de>
2021-01-07 10:01:15 +0100

    docs: Add docblocks for Source methods
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/source.rb
+++ app/models/source.rb
@@ -17,20 +17,29 @@ class Source < ApplicationRecord
     name
   end
 
+  # Checks whether the corresponding source gem was bundled with mirr.OS.
+  # @return [TrueClass, FalseClass] True if the source was included on install.
   def pre_installed?
     MirrOSApi::Application::DEFAULT_SOURCES.include?(slug)
   end
 
+  # Returns the class constant for this source's Engine class.
+  # @return [Class] The Engine class constant.
   def engine_class
     find_source_class('Engine')
   end
 
+  # Returns the class constant for this source's Hooks implementation.
+  # @return [Class] The Hooks class constant.
   def hooks_class
     @hooks_class ||= find_source_class('Hooks')
   end
 
   private
 
+  # Find and constantize the given class from this source's gem.
+  # @param [Object] type `Engine` or `Hooks`
+  # @return [Class] The class constant for the given type.
   def find_source_class(type)
     klass_name = "#{id.camelize}::#{type}"
 
