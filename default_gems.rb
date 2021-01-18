c8461480f92c3c2c616e09fcfce725561f656cbd
Tobias Grasse <tg@glancr.de>
2021-01-18 11:08:56 +0100

    build: Add gem `redis`
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- default_gems.rb
+++ default_gems.rb
@@ -12,6 +12,7 @@ source 'https://rubygems.org' do
   gem 'puma', '~> 4.0.1'
   gem 'bootsnap', '>= 1.1.0', require: false
   gem 'rack-cors'
+  gem 'redis'
   group :development, :test do
     # Call 'byebug' anywhere in the code to stop execution and get a debugger console
     gem 'byebug', platforms: %i[mri mingw x64_mingw]
