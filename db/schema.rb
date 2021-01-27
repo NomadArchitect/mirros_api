37c4b11fc7700003c236467b120e3aa1ae287606
Tobias Grasse <tg@glancr.de>
2021-01-27 13:56:46 +0100

    feat: Add index on settings.slug column
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_115751) do
+ActiveRecord::Schema.define(version: 2021_01_27_092224) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -282,6 +282,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_115751) do
     t.string "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_settings_on_slug", unique: true
   end
 
   create_table "source_instances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
