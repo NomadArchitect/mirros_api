81676cb574afdab6e7b8c01edfb6be280d60eeef
Tobias Grasse <tg@glancr.de>
2021-02-16 10:57:13 +0100

    feat: Add sub-class WidgetInstanceStyles
    Helper for a WidgetInstance’s style property. Allows structured access and validation while keeping all values in a single column.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

---
+++ app/models/instances/widget_instance_styles.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Sub-model for style settings of a WidgetInstance.
+class WidgetInstanceStyles
+  include StoreModel::Model
+
+  attribute :font_color, :string, default: ''
+  attribute :background_blur, :boolean, default: false
+  attribute :font_size, :integer, default: 100
+
+  validates :font_color,
+            allow_blank: true,
+            format: {
+              with: /\#([0-9a-f]{6})/,
+              message: 'must be a hex value, e.g. #ff0000'
+            }
+  validates :font_size, numericality: { only_integer: true, greater_than_or_equal_to: 100 }
+  validates :background_blur,
+            inclusion: {
+              in: [true, false],
+              message: 'must be one of [true, false]'
+            }
+  # FIXME: String/Array values for the background_blur attribute are coerced to Boolean
+  #  at some point in the attribute setup stack, which lets them pass validation. Only `null` fails.
+end
