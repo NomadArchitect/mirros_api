898ca40dee96c71c80366a4e1a4d8c18d3185ee0
Tobias Grasse <tg@glancr.de>
2021-02-16 10:46:00 +0100

    test: Add WidgetInstanceTest cases for styles
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- test/models/instances/widget_instance_test.rb
+++ test/models/instances/widget_instance_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class WidgetInstanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class
+WidgetInstanceTest < ActiveSupport::TestCase
+  fixtures :boards, :widgets, :widget_instances
+
+  test 'creates default styles if client does not provide them' do
+    wi = WidgetInstance.new
+    assert wi.styles.present? && wi.styles.instance_of?(WidgetInstanceStyles)
+  end
+
+  test 'validates style properties' do
+    valid_wi = widget_instances(:valid_styles)
+    valid_wi.save
+
+    assert valid_wi.valid?
+
+    invalid_wi = widget_instances :invalid_styles
+    invalid_wi.save
+
+    assert_invalid invalid_wi,
+                   font_color: 'must be a hex value, e.g. #ff0000',
+                   font_size: 'must be greater than or equal to 100',
+                   background_blur: 'must be one of [true, false]'
+  end
 end
