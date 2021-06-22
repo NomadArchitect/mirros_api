5a8e2185c2b360ea458ac2d03cb3170108562326
Tobias Grasse <tg@glancr.de>
2021-06-22 09:28:40 +0200

    style: Code formatting
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/rule_manager/board_scheduler.rb
+++ app/lib/rule_manager/board_scheduler.rb
@@ -14,22 +14,22 @@ module RuleManager
 
       Rufus::Scheduler.singleton.cron '* * * * *', tag: RULE_EVALUATION_TAG do
         rule_applied = false
-          Board.all.each do |board|
-          # TODO: Extend logic for rule sets in each board.
+        Board.all.each do |board|
+        # TODO: Extend logic for rule sets in each board.
 
-          # Date-based rules take precedence over recurring rules.
-          if board.rules.where(provider: 'system', field: 'dateAndTime').any?(&:evaluate)
-            Setting.find_by(slug: :system_activeboard).update(value: board.id)
-            rule_applied = true
-            break
-          end
-          # TODO: this only gets time-based rules for the board and runs them in sequential order.
-          if board.rules.where(provider: 'system', field: 'timeOfDay').any?(&:evaluate)
-            Setting.find_by(slug: :system_activeboard).update(value: board.id)
-            rule_applied = true
-            break
-          end
+        # Date-based rules take precedence over recurring rules.
+        if board.rules.where(provider: 'system', field: 'dateAndTime').any?(&:evaluate)
+          Setting.find_by(slug: :system_activeboard).update(value: board.id)
+          rule_applied = true
+          break
         end
+        # TODO: this only gets time-based rules for the board and runs them in sequential order.
+        if board.rules.where(provider: 'system', field: 'timeOfDay').any?(&:evaluate)
+          Setting.find_by(slug: :system_activeboard).update(value: board.id)
+          rule_applied = true
+          break
+        end
+      end
 
         Setting.find_by(slug: :system_activeboard).update(value: Board.first.id) unless rule_applied
       end
