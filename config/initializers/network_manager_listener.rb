c5d955b27822b0812e99a49ef0c686c0c8f7da37
Tobias Grasse <tg@glancr.de>
2021-11-03 11:23:03 +0100

    fix: Use UpdateConnectivityStatusJob in n-m listener initializer
    macOS hosts only had a stub IP change check anyway, so we can remove the cruft and assume an always-on network stack on macOS.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/network_manager_listener.rb
+++ config/initializers/network_manager_listener.rb
@@ -11,16 +11,8 @@ if OS.linux?
   # As of N-M 1.10, the connectivity status doesn't seem to be pushed reliably
   # over DBus. This ensures the StateCache is updated at least every minute.
   # TODO: Revisit once we can use 1.16 or later on Core.
-  if NetworkManager::Commands.instance.connectivity_check_available?
-    s.every '60s', tag: 'network-connectivity-check', overlap: false do
-      StateCache.put :connectivity, NetworkManager::Commands.instance.connectivity
-    end
-  end
-else
-  # On other hosts, we schedule network status check jobs.
-  s.every '30s', tag: 'network-status-check', overlap: false do
-    System.check_network_status
-    System.push_status_update
-  end
-
+  Sidekiq.set_schedule UpdateConnectivityStatusJob.name,
+                       interval: '30s',
+                       overlap: false,
+                       class: UpdateConnectivityStatusJob
 end
