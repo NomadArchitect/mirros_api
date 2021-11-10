b45fe10b26382492bffda899c868baee21304f1e
Tobias Grasse <tg@glancr.de>
2021-11-10 16:30:03 +0100

    fix: Only run n-m signal listener initializer if running in snap
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/custom/network_manager_listener.rb
+++ config/initializers/custom/network_manager_listener.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if OS.linux?
+if OS.linux? && System.running_in_snap?
   # On linux hosts, we utilize NetworkManager signal listeners.
   listeners = NetworkManager::SignalListeners.instance
   listeners.add_permanent_listeners
