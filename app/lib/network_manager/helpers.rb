34b35404f12a88759e6709f4afe324fa59d8de67
Tobias Grasse <tg@glancr.de>
2021-12-13 20:50:52 +0100

    refactor: Remove ununsed NetworkManager helper methods
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/network_manager/helpers.rb
+++ app/lib/network_manager/helpers.rb
@@ -3,19 +3,6 @@
 module NetworkManager
   # Helpers for NetworkManager-related routines. Used in SignalListeners and in Commands.
   module Helpers
-    include Constants
-    def map_state(state)
-      NmState.constants.select do |c|
-        NmState.const_get(c).eql? state
-      end.pop
-    end
-
-    def map_connectivity(connectivity_state)
-      NmConnectivityState.constants.select do |c|
-        NmConnectivityState.const_get(c).eql? connectivity_state
-      end.pop
-    end
-
     def retry_wrap(max_attempts: 3, &block)
       attempts = 0
       begin
