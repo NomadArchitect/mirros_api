ab0333dc5ef616ab3678e1c959f193724f372d55
Tobias Grasse <tg@glancr.de>
2021-12-13 21:54:16 +0100

    refactor: Remove connectivity status update job
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/custom/network_manager_listener.rb
+++ config/initializers/custom/network_manager_listener.rb
@@ -7,14 +7,4 @@ if OS.linux? && System.running_in_snap?
   listeners.listen
 
   at_exit { listeners.quit }
-
-  # As of N-M 1.10, the connectivity status doesn't seem to be pushed reliably
-  # over DBus. This ensures the StateCache is updated at least every minute.
-  # TODO: Revisit once we can use 1.16 or later on Core.
-  Sidekiq.set_schedule UpdateConnectivityStatusJob.name,
-                       {
-                         interval: '30s',
-                         overlap: false,
-                         class: UpdateConnectivityStatusJob
-                       }
 end
