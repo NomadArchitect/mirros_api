6b633ffb662135fff792ec9cda02de728ce2320a
Tobias Grasse <tg@glancr.de>
2022-01-10 13:24:59 +0100

    fix: Rename PoP migration class
    Consistent with other files, should use table plural name.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- db/migrate/20201222103534_add_pop_to_current_weather_entries.rb
+++ db/migrate/20201222103534_add_pop_to_current_weather_entries.rb
@@ -1,4 +1,4 @@
-class AddPopToCurrentWeatherEntry < ActiveRecord::Migration[5.2]
+class AddPopToCurrentWeatherEntries < ActiveRecord::Migration[5.2]
   def change
     add_column :group_schemas_current_weather_entries, :pop, :decimal, scale: 2
   end
