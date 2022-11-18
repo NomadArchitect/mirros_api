eb71afe287d3bf57c8ff0a1a77c734b67848d85e
Tobias Grasse <tg@glancr.de>
2022-11-18 14:16:32 +0100

    fix: Do not set default locale in initializer unless running in server context
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- config/initializers/custom/set_default_locale.rb
+++ config/initializers/custom/set_default_locale.rb
@@ -1,3 +1,6 @@
+# Abort running this in rake tasks as the settings table might not exist yet.
+return unless Rails.const_defined? 'Server'
+
 Rails.configuration.after_initialize do
   configured_locale = ::Setting.value_for('system_language')&.slice(0, 2)
   I18n.default_locale = configured_locale&.present? ? configured_locale.to_sym : :en
