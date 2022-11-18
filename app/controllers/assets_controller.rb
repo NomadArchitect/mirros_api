3caf04f5c0a4c67dd47c4ced66f9722cad55b5b9
Tobias Grasse <tg@glancr.de>
2022-11-18 16:29:12 +0100

    fix: Ensure asset checks the given version constraint
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -6,6 +6,10 @@ class AssetsController < ApplicationController
     gem = Gem.loaded_specs["mirros-#{params[:extension_type].singularize}-#{params[:extension]}"] || Gem.loaded_specs[params[:extension]]
     return head :not_found if gem.nil?
 
+    if params[:version]
+      return head :not_found unless gem.version.to_s.eql?(params[:version])
+    end
+
     extension_path = gem.full_gem_path
     file_path = "#{extension_path}/app/assets/#{params[:asset_type]}/#{params[:file]}"
 
