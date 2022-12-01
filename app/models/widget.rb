b34872196d15532046590e7031a60b429936c0bd
Tobias Grasse <tg@glancr.de>
2022-12-01 13:01:31 +0100

    refactor: Move common base functionality to ExtensionBase
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,35 +1,20 @@
-class Widget < ApplicationRecord
+class Widget < ExtensionBase
+  # Primary key must be set in the inheriting class.
   self.primary_key = 'slug'
+  # Use slugged version of name as a predictable ID.
+  include FriendlyId
+  friendly_id :name, use: :slugged
 
   has_many :widget_instances, dependent: :destroy
   belongs_to :group, optional: true
 
-  validates :name, presence: true, uniqueness: true
   validates :title, presence: true
   validates :description, presence: true
   validates :version, presence: true, format: /[0-9].[0-9].[0-9]/
   validates :download, presence: true
 
-  extend FriendlyId
-  friendly_id :name, use: :slugged
-
-  def to_s
-    name
-  end
-
   def pre_installed?
     MirrOSApi::Application::DEFAULT_WIDGETS.include?(slug)
   end
 
-  def engine_class
-    engine_name = "#{id.camelize}::Engine"
-
-    begin
-      engine = engine_name.constantize
-    rescue NameError
-      engine = "Mirros::Widget::#{engine_name}".safe_constantize
-    end
-
-    engine
-  end
 end
