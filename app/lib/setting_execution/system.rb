44ca94fbab6bbbdeb498a55c2e86833cc251d967
Tobias Grasse <tg@glancr.de>
2023-01-16 09:48:14 +0100

    #220: Use DBus API for timezone with fresh system bus instance
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- app/lib/setting_execution/system.rb
+++ app/lib/setting_execution/system.rb
@@ -9,17 +9,13 @@ module SettingExecution
     # @param [String] tz_identifier A valid timezone identifier, see zoneinfo.
     def self.timezone(tz_identifier)
       if OS.linux?
-        # FIXME: Use CLI until https://bugs.launchpad.net/snappy/+bug/1650688 is fixed
-        # sysbus = DBus.system_bus
-        # timedated_service = sysbus['org.freedesktop.timedate1']
-        # timedated_object = timedated_service['/org/freedesktop/timedate1']
-        # timedated_interface = timedated_object['org.freedesktop.timedate1']
-        line = Terrapin::CommandLine.new('timedatectl', 'set-timezone :timezone')
-        line.run(timezone: tz_identifier)
-        # Ref: https://www.freedesktop.org/wiki/Software/systemd/timedated/
+        sysbus = DBus::ASystemBus.new
+        timedated_interface = sysbus['org.freedesktop.timedate1']['/org/freedesktop/timedate1']['org.freedesktop.timedate1']
+        # @see https://www.freedesktop.org/wiki/Software/systemd/timedated/
         # tz needs to be a valid timezone from /usr/share/zoneinfo/zone.tab
         # bool: User interaction
-        # timedated_interface.SetTimezone(tz_identifier, false)
+        # noinspection RubyResolve
+        timedated_interface.SetTimezone(tz_identifier, false)
         #::System.toggle_timesyncd_ntp(true)
         # TODO: See if this needs additional error handling, either here or in controller
       else
